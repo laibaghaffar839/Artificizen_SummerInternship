490d0c3fac686a537e6c93d95c3cd99c0f9a4956
Laiba Shahzadi <laibaghaffar839@gmail.com>
2026-07-13 16:37:10 +0500

    Delete Week 2/Task3.py

--- Week 2/Task3.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/items")
-def get_items(skip: int = 0, limit: int = 10):
-    items = [f"Item {i}" for i in range(1, 101)]  # Fake list of 100 items
-    return items[skip: skip + limit]
